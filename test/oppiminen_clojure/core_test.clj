3fe8487c23913ada80d710f1481266d19840f68b
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2018-04-28 17:20:09 +0300

    Scaffold
    lein new app oppiminen-clojure

---
+++ test/oppiminen_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns oppiminen-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [oppiminen-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
